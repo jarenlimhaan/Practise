cc5fa9e5d35987dcdea7cd9f3e27f7a629e6e06a
Jaren <kaylajaren@gmail.com>
2025-05-22 17:03:44 +0800

    update

---
+++ leetcode/general/easy/middle_of_the_linked_list.py
@@ -0,0 +1,9 @@
+def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head or not head.next:
+        return head
+    
+    slow, fast = head, head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
